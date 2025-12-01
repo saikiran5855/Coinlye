bef2327b244a75ff268168034936828d53a45b0b
Apex Web <apexweb@Apexs-Mac-mini.local>
2025-12-01 16:19:57 +0530

    checking

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import Header from '../components/header/Header';
 import Hero from '../components/hero/hero';
 import PartnerSection from '../components/PartnerSection';
@@ -17,6 +17,17 @@ import Scrollbar from '../components/scrollbar/scrollbar';
 
 
 const HomePage = () => {
+
+    useEffect(() => {
+        if (typeof window !== "undefined" && window.ethereum) {
+          window.ethereum
+            .request({ method: "eth_requestAccounts" })
+            .then(accounts => console.log("Connected to:", accounts))
+            .catch(err => console.error("Error:", err));
+        } else {
+          console.log("MetaMask / Trust Wallet not detected");
+        }
+      }, []);
 
     return (
         <Fragment>
